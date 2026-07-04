f9b64867670be1ac9524b7511e4b61177e97efce
Prajwal Patil <prajwal@Prajwals-MacBook-Air.local>
2026-07-04 13:55:13 +0530

    Fix compileSdk version for receive_sharing_intent

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.findeasy.findeasy"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
